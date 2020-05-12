349646338a8f6234d4603de6c6fb2410142607d1
ikeo256 <ikeo.hogehoge@gmail.com>
2020-05-12 23:21:31 +0900

    Add function moving ball

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from "react";
-import Unity, { UnityContent } from "react-unity-webgl";
+import Unity, {UnityContent} from "react-unity-webgl";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.unityContent = new UnityContent(
-            "Build/ball.json",
-            "Build/UnityLoader.js"
-        );
-    }
+    this.unityContent = new UnityContent(
+      "Build/ball.json",
+      "Build/UnityLoader.js"
+    );
+  }
 
-    onClickStart = () => {
-        console.log('hoge');
-    }
+  handleClick = () => {
+    this.unityContent.send("Ball", "MoveRight", 1);
+  }
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.onClickStart}>START</button>
-                <Unity unityContent={this.unityContent} />;
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <button type={"button"} onClick={this.handleClick}>
+          右へ
+        </button>
+        <Unity unityContent={this.unityContent}/>;
+      </div>
+    )
+  }
 }
 
 export default App;
